d340471e13a4bf29ccb9ce0eb125387d0566cb2e
Dmitry <Dmitry@DIMA>
2022-05-28 16:05:41 +0300

    Добавьте файлы проекта.

---
+++ list/list.cpp
@@ -0,0 +1,16 @@
+﻿#include "Header.h"
+
+int main() {
+    list<int> list;
+    list.push_back(5);
+    list.push_back(6);
+    list.push_back(7);
+    for (size_t i = 0; i != list.size(); ++i)
+        std::cout << list[i] << std::endl;
+
+    list.pop_back();
+
+    std::cout << "====" << std::endl;
+    for (size_t i = 0; i != list.size(); ++i)
+        std::cout << list[i] << std::endl;
+}
